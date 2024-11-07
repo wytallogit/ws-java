004aa734e3bd1b55f5f305f83210891ce19265ad
Wytallo Vinicius <157238340+wytallogit@users.noreply.github.com>
2024-11-06 23:40:19 -0300

    uup

---
+++ estrutura-condicional/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int hora = 22;
+
+        if(hora < 12) {
+            System.out.println("Bom dia");
+            return;
+        };
+
+        System.out.println("Boa noite");
+    }
+}
